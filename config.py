77a20409502e8b22456799923b0c9fdba63c9ece
Rakshitha-33 <4mu22cs069@mysururoyal.org>
2026-06-06 12:10:10 +0530

    Initial MarketHub project upload

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
+import os
+import urllib.parse
 class Config:
 
     SECRET_KEY = 'markethub'
 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Nishu%401412@localhost/markethub'
+    
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+   
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_very_secret_key_123'
+    
+    password = urllib.parse.quote_plus('rakshitha@3302')
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:{password}@localhost/markethub_db'
+    
+
